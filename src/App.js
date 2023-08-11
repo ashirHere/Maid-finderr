47309150acd2d87db1b5b83cd5841515d8936fe7
Agha Mohammad Huzaifa Khan <71359430+AghaHuzaifaKhan@users.noreply.github.com>
2023-08-11 14:34:08 +0000

    N0tHuzaifa Changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
   return (
+    <>
+    <img src = {logo} className='logo' ></img>
+    <h1>KAAM WALI</h1>
+    <nav>
+    <li>Home</li>
+    <li>Contact</li>
+    <li>About us </li>
+    </nav>
+    <Mybutton>Login</Mybutton>
+    
     <div className="App">
-      <h1>Syed Ashir Shoaib</h1>
-    </div>
+      
+        
+
+        
+        </div>
+        </> 
+
+   
   );
 }
+function Mybutton() {
+  return (
+<div >
+  <button>
+   Log In
+  </button>
+
+</div>
+  )
+}
+
+
 
 export default App;
+
+
