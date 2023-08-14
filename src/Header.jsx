5f7ea44b0ba23c342b983a69744c048a64b1f3d6
wellashirr <ashirshoaib2001@gmail.com>
2023-08-14 07:22:23 +0500

    by ashir

---
+++ src/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+function Header() {
+    return (
+        <div>
+            <div>
+                <h1>KAAM WALI</h1>
+                <nav>
+                    <li>Home</li>
+                    <li>Contact</li>
+                    <li>About us </li>
+                    <li>About us </li>
+                </nav>
+            </div>
+        </div>
+    );
+}
+
+
+export default Header;
